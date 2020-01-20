8c778cf19ba2a2b2567f63898c7822681f6a280d
RhiannonAnna <annaandrzejewskagodecka@o2.pl>
2020-01-20 21:11:45 +0100

    SQL procedures

---
+++ TimeTracking/CodeBase/SQL/StoredProcedures/USP_InsertTimeTrackingLog.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[USP_InsertTimeTrackingLog]
+	(
+    @Guid VARCHAR(30)
+    , @Description NVARCHAR(MAX)
+    , @StartTime DateTime
+	, @StartMethodTime DateTime
+	, @EndTime DateTime
+	, @TotalTime Decimal
+	, @MethodTime Decimal
+	, @Job NVARCHAR(300)
+	, @JobTypeId SMALLINT
+	) AS
+
+BEGIN
+		SET NOCOUNT ON;
+		
+			BEGIN
+				INSERT INTO [dbo].[TimeTrackingLogs] ([Guid], [Description], [StartTime], [StartMethodTime], [EndTime], [TotalTime], [MethodTime], [Job], [JobTypeId])
+			
+				VALUES (@Guid, @Description, @StartTime, @StartMethodTime, @EndTime, @TotalTime, @MethodTime, @Job, @JobTypeId)
+
+(SELECT  SCOPE_IDENTITY());
+			END
+
+		SET NOCOUNT OFF
+	END
---
+++ TimeTracking/CodeBase/SQL/StoredProcedures/USP_SelectAllTimeTrackingLogsForSelectedJob.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[USP_SelectAllTimeTrackingLogsForSelectedJob]
+
+@Job NVARCHAR(300)
+AS	
+
+BEGIN
+		SET NOCOUNT ON;
+		
+			BEGIN
+
+				SELECT 
+				  [ID]
+				, [Guid]
+				, [Description]
+				, [StartTime]
+				, [StartMethodTime]
+				, [EndTime]
+				, [TotalTime]
+				, [MethodTime]
+				, [Job]
+				, [JobTypeId]
+				FROM [dbo].[TimeTrackingLogs]
+				WHERE [Job] = @Job
+				
+			END
+
+		SET NOCOUNT OFF
+	END
