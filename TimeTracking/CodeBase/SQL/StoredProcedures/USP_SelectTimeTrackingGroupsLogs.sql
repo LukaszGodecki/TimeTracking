916f188c25912e6cbdc0cfa966742b5f63f36c78
Anna Andrzejewska-Godecka <46266571+RhiannonAnna@users.noreply.github.com>
2020-01-20 21:11:48 +0100

    SQL procedures (#2)

---
+++ TimeTracking/CodeBase/SQL/StoredProcedures/USP_SelectTimeTrackingGroupsLogs.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[USP_SelectTimeTrackingGroupsLogs]
+
+@Guid	VARCHAR(30)
+,@Job	NVARCHAR(300)
+,@JobTypeId	SMALLINT
+AS	
+
+BEGIN
+		SET NOCOUNT ON;
+		
+			BEGIN
+
+				SELECT 
+				[Guid]
+				, [Job]
+				, [JobTypeId]
+				, Count(ID) AS [Count]
+				FROM [dbo].[TimeTrackingLogs]
+				WHERE [Job] = ISNULL(@Job, [Job]) AND [Guid] = ISNULL(@Guid, [Guid]) AND [JobTypeId] = ISNULL(@JobTypeId, [JobTypeId])
+				GROUP BY [Guid], [Job], [JobTypeId]
+				
+			END
+
+		SET NOCOUNT OFF
+	END
---
+++ TimeTracking/CodeBase/SQL/Tables/TimeTrackingLogs.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[TimeTrackingLogs]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Guid]	VARCHAR(30)	NOT NULL,
+	[Description]	NVARCHAR(MAX)	NOT NULL,
+	[StartTime]	DateTime	NULL,
+	[StartMethodTime]	DateTime	NULL,
+	[EndTime]	DateTime	NOT NULL,
+	[TotalTime]	Decimal	NULL,
+	[MethodTime]	Decimal	NULL,
+	[Job]	NVARCHAR(300)	NOT NULL,
+	[JobTypeId]	SMALLINT	NOT NULL, 
+    CONSTRAINT [FK_TimeTrackingLogs_JobTypes] FOREIGN KEY ([JobTypeId]) REFERENCES [dbo].[JobTypes]([Id])
+)
